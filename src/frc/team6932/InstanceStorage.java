3784ee621c6049c129d6ac4652ff08f3f724ae41
Isaac <isaacalich8401@gmail.com>
2018-02-17 16:40:15 -0500

    It works! (not very well)

--- src/frc/team6932/InstanceStorage.java
+++ src/frc/team6932/InstanceStorage.java
@@ -1,6 +1,7 @@
 package frc.team6932;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PWMTalonSRX;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -17,9 +18,12 @@ public class InstanceStorage {
     // Motor controllers
     public Spark leftDrive = new Spark(0);
     public Spark rightDrive = new Spark(1);
-    public Spark rightCimCubeMotors = new Spark(8);
-    public Spark leftCimCubeMotors = new Spark(9);
-    public Spark redlineCubeMotors = new Spark(3);
+    public Spark rightCimCubeMotor = new Spark(9);
+    public Spark leftCimCubeMotor = new Spark(8);
+    public Spark rightRedlineCubeMotor = new Spark(6);
+    public Spark leftRedlineCubeMotor = new Spark(7);
+    public PWMTalonSRX rightCimGrabberMotor = new PWMTalonSRX(5);
+    public PWMTalonSRX leftCimGrabberMotor = new PWMTalonSRX(4);
     public DifferentialDrive drive = new DifferentialDrive(leftDrive, rightDrive);
 
     // Joystick
@@ -29,7 +33,7 @@ public class InstanceStorage {
     public double forwardAxis = joystick.getRawAxis(1);
 
     // Sensor variables
-    PowerDistributionPanel pdp = new PowerDistributionPanel();
+    PowerDistributionPanel pdp = new PowerDistributionPanel(0);
 
     public static InstanceStorage getInstance() {
         return instance;
--- src/frc/team6932/Robot.java
+++ src/frc/team6932/Robot.java
@@ -34,7 +34,7 @@ public class Robot extends TimedRobot {
     @Override
     public void robotPeriodic() {
         // Smart Dashboard
-        SmartDashboard.putBoolean("Cube in Robot: ", true);
+        SmartDashboard.putBoolean("Cube in Robot", true);
     }
 
     /**
@@ -79,8 +79,20 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void teleopPeriodic() {
-        vars.rightCimCubeMotors.set(vars.forwardAxis);
-        vars.leftCimCubeMotors.set(vars.forwardAxis);
+        vars.leftCimCubeMotor.set(0.53); // Oddball motor
+        vars.rightCimCubeMotor.set(0.5);
+        vars.rightRedlineCubeMotor.set(0.43); // Oddball gearbox?
+        vars.leftRedlineCubeMotor.set(0.4);
+        vars.rightCimGrabberMotor.set(0.4);
+        vars.leftCimGrabberMotor.set(0.4);
+        vars.drive.arcadeDrive(vars.forwardAxis, vars.sideAxis);
+    }
+
+    public void pickup() {
+        vars.leftCimCubeMotor.set(0.53); // Oddball motor
+        vars.rightCimCubeMotor.set(0.5);
+        vars.rightCimGrabberMotor.set(0.4);
+        vars.leftCimGrabberMotor.set(0.4);
     }
 
     /**
