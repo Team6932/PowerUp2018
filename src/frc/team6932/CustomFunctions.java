6bda51660456fc02a13052a3e1b56020401f61c2
Isaac <isaacalich8401@gmail.com>
2018-02-19 12:49:28 -0500

    Tone down cube transport motors a bit & update copyright

--- src/frc/team6932/CustomFunctions.java
+++ src/frc/team6932/CustomFunctions.java
@@ -11,8 +11,8 @@ public class CustomFunctions {
     public void setWithCorrections(double leftCimGrabber, double rightCimGrabber, double leftCimCube, double rightCimCube, double leftRedlineCube, double rightRedlineCube) {
         vars.leftCimGrabberMotor.set(0.4 * leftCimGrabber);
         vars.rightCimGrabberMotor.set(0.4 * rightCimGrabber);
-        vars.leftCimCubeMotor.set((0.5 * leftCimCube) + 0.03); // Oddball motor
-        vars.rightCimCubeMotor.set(0.5 * rightCimCube);
+        vars.leftCimCubeMotor.set((0.4 * leftCimCube) + 0.03); // Oddball motor
+        vars.rightCimCubeMotor.set(0.4 * rightCimCube);
         vars.leftRedlineCubeMotor.set(0.4 * leftRedlineCube);
         vars.rightRedlineCubeMotor.set((0.4 * rightRedlineCube) + 0.03); // Oddball motor/gearbox
     }
