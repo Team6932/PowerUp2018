2ef2408d0ff60f1d5a1bf381071c2a2655089216
Isaac <isaacalich8401@gmail.com>
2018-02-19 12:55:14 -0500

    Fix spacing on license and clean out missed comment

--- src/frc/team6932/Robot.java
+++ src/frc/team6932/Robot.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -12,7 +12,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 
 public class Robot extends TimedRobot {
 
-    // Get the Instance Storage Object
+    // Get storage objects
     private InstanceStorage vars = InstanceStorage.getInstance();
     private CustomFunctions func = CustomFunctions.getInstance();
     private DashController dash = DashController.getInstance();
@@ -71,9 +71,6 @@ public class Robot extends TimedRobot {
         vars.drive.arcadeDrive(vars.verticalAxis, vars.horizontalAxis);
     }
 
-    /**
-     * This function is called periodically during test mode.
-     */
     @Override
     public void testPeriodic() {
 
